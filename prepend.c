72af4361f5f655967ae5643361211debd973f7e0
Holland Naftali Nyadimo <naftaliholland01@gmail.com>
2023-08-21 18:46:03 +0300

    A function that prepends "/bin/" to a command that is not in the current directory

--- prepend.c
+++ prepend.c
@@ -1,5 +1,8 @@
 #include "main.h"
-
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 /**
  * prepend - this function prepends raw passed commands
  * with /bin/
@@ -10,6 +13,28 @@
 
 char *prepend(char *command)
 {
+	char *command_path;
+	int i;
 
-	return (command);
+	/**
+	 * check if the file is in the current directory
+	 */
+	if (access(command, F_OK) == 0)
+		return (command);
+
+	i = 5;
+	/**
+	 * i is initially 5 because o "/bin/" has four characters
+	 */
+	while (command[i] != '\0')
+		i++;
+
+	command_path = malloc(sizeof(char) * i);
+	if (command_path == NULL)
+		return (NULL);
+
+	strcpy(command_path, "/bin/");
+	strcat(command_path, command);
+
+	return (command_path);
 }
