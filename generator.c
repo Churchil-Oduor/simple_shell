31a17f586f9e8a7e008bfab5596eb1944f6c5e5b
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-08-22 09:51:08 +0300

    Corrected the generator function to only take in one parameter-> command, because at no point are we using the environment variables

--- generator.c
+++ generator.c
@@ -9,12 +9,11 @@
  * The string at index 0 is the file path location.
  *
  * @command: is the passed command requiring prepending.
- * @env_vars: is the list of environment variables.
  *
  * Return: returns an array of strings that are NULL terminated.
  */
 
-char **generator(char *command, char *const env_vars)
+char **generator(char *command)
 {
 	char *buffer, *token, *hold_first_token, **args;
 	const char delim[] = " ";
--- main.h
+++ main.h
@@ -4,12 +4,12 @@
 char *prepend(char *command);
 void interactive_mode(char *file_name, char *const env_vars[]);
 void non_interactive_mode(char *file_name, char *const env_vars[]);
-char **generator(char *command, char *const env_vars);
+char **generator(char *command);
 int execute(char **args, char *const env_vars[]);
 
 int total_array_length(char *str);
 int _strlen(char *str);
 int does_path_exist(char *str);
-
+void free_args(char **args);
 
 #endif
