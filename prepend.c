988544abf549c86686d404ae214156a77833406c
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-08-22 09:45:57 +0300

    Modified prepend to generate null terminated string and also to count the total lenght required

--- prepend.c
+++ prepend.c
@@ -15,7 +15,7 @@
 char *prepend(char *command)
 {
 	char *command_path;
-	int i;
+	int i, len;
 
 	/**
 	 * check if the file is in the current directory
@@ -27,15 +27,21 @@ char *prepend(char *command)
 	/**
 	 * i is initially 5 because o "/bin/" has four characters
 	 */
-	while (command[i] != '\0')
-		i++;
 
-	command_path = malloc(sizeof(char) * i);
+	i += _strlen(command);
+
+	command_path = malloc(sizeof(char) * (i + 1));
+	/**
+	 * added 1 to accomodate the null terminator and avoiding memory leak
+	 */
+
 	if (command_path == NULL)
 		return (NULL);
 
 	strcpy(command_path, "/bin/");
 	strcat(command_path, command);
+	len = _strlen(command_path);
+	command_path[len] = '\0'; /**Null terminator**/
 
 	return (command_path);
 }
