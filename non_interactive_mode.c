31ebbc73dab6ac957d30e124c5d95147f63caa82
Churchil-Oduor <churchilokechoduor@gmail.com>
2023-08-23 14:05:01 +0300

    Pushing the non-interactive module

--- non_interactive_mode.c
+++ non_interactive_mode.c
@@ -1,6 +1,8 @@
 #include <unistd.h>
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
+#define BUFFER_SIZE 1024
 
 /**
  * non_interactive_mode - the function managing interactive mode.
@@ -13,5 +15,18 @@
 
 void non_interactive_mode(char *file_name, char *const env_vars[])
 {
-	/**code**/
+	char *command, **args;
+	size_t read_bytes;
+
+	read_bytes = read(STDIN_FILENO, command, BUFFER_SIZE);
+
+	if (read_bytes < 0)
+	{
+		perror("Error reading command from terminal");
+		exit(100);
+	}
+	command[read_bytes - 1] = '\0';
+	args = generator(command);
+	execute(args, env_vars);	
 }
+
